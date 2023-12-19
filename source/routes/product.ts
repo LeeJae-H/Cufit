ee58c7f7932120c38c256e60fde6cb25f74cf049
Wonyoung Jang <nex5turbo@gmail.com>
2023-12-19 11:49:58 +0900

    find -> findOne

--- source/routes/product.ts
+++ source/routes/product.ts
@@ -210,7 +210,7 @@ router.get("/detail/:productId", async (req, res) => {
   let isLiked: Boolean = await Like.isExist(productId, uid, type);
   let isWished: Boolean = await Wish.isExist(productId, uid, type);
   let isPurchased: Boolean = await Order.isExist(productId, uid, type);
-  let review = await Review.find({uid, productId});
+  let review = await Review.findOne({uid, productId});
   
   res.status(200).json({
     creator: user,
