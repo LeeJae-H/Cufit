25b8c25c528adb058a0231dc91e74e1dd7fbd214
LeeJaehyung <540900@naver.com>
2024-01-30 11:02:59 +0900

    edit_image.controller

--- src/controllers/image.controller.ts
+++ src/controllers/image.controller.ts
@@ -1,24 +1,33 @@
 import { Request, Response } from 'express';
 import s3 from '../config/storage';  
-import { v4 as uuidv4 } from 'uuid';
 
 export const uploadImage = async (req: Request, res: Response) => {
   try {
     if (!req.file) {
       return res.status(400).json({ message: 'No file uploaded' });
     }
+    const type = req.body.type;
+    
     const file = req.file;
     const { originalname, buffer } = file;
-    const key = `${uuidv4()}_${originalname}`;
+    const fileType = originalname.split('.').pop();
+
     const params = {
         Bucket: "cufit-staging-image-bucket",
-        Key: key,
+        Key: `${type}/${Date.now()}.${fileType}`,
         Body: buffer,
         ContentType: file.mimetype,
     };
     const uploadResult = await s3.upload(params).promise();
-    const imageKey = uploadResult.Key;  
-    res.json(imageKey);  
+    const imageUrl = uploadResult.Location;  
+    res.json({
+      statusCode: 0,
+      message: "Image uploaded successfully.",
+      result: {
+        url: imageUrl,
+        type: type
+      }
+    });  
 
 } catch (error) {
     console.error(error);
@@ -27,13 +36,20 @@ export const uploadImage = async (req: Request, res: Response) => {
 };
 
 export const deleteImage = async (req: Request, res: Response) => {
-  const key = req.body.key;
+  const fileName = req.body.fileName;
+  const type = req.body.type;
+
   try {
     const result = await s3.deleteObject({ 
         Bucket: 'cufit-staging-image-bucket', 
-        Key: key
+        Key: `${type}/${fileName}.png`
     }).promise();
-    res.json(result);
+
+    res.json({
+      statusCode: 0,
+      message: "Success",
+      result
+    });
 
   } catch (error) {
     console.error(error);
