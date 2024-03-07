1f596a537ab3ec6e0190fa6b0dfb2cf6a56419de
LeeJaehyung <540900@naver.com>
2024-03-08 06:20:36 +0900

    edit multer

--- src/routes/image.route.ts
+++ src/routes/image.route.ts
@@ -1,6 +1,9 @@
 import express from 'express';
-import upload from '../middlewares/multer';
 import { uploadImage, deleteImage } from '../controllers/image.controller';
+
+import multer from 'multer'; // multipart/form-data
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 const router = express.Router();
 
