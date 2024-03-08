aed5af2fd08fa4570a89e31f48bf4ef5c954faf9
Wonyoung Jang <nex5turbo@gmail.com>
2024-03-08 15:52:32 +0900

    search populate

--- src/models/guideline.model.ts
+++ src/models/guideline.model.ts
@@ -228,6 +228,11 @@ GuidelineSchema.statics.searchbyTitleOrTag = async function(keyword: string) {
       { title: { $regex: new RegExp(keyword, 'i') } }
     ],
   })
+  .populate('likedCount')
+  .populate('wishedCount')
+  .populate('usedCount')
+  .populate('authStatus')
+  .populate('creator');
   return result;  
 }
 
