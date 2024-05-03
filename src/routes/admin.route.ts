54b1714fa3395b1ff14e1087a07d628662390f10
LeeJaehyung <540900@naver.com>
2024-05-03 20:16:49 +0900

    +apis, +main.route.ts

--- src/routes/admin.route.ts
+++ src/routes/admin.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { postStatus, getContent, getContents, postContents, 
-    getFaqs, getProducts, postFaqAnswer, postAuth
+    getFaqs, getProducts, postFaqAnswer, postAuth, uploadTagList, modifyTagList, uploadPhotoZone, uploadGuideline
 } from '../controllers/admin.controller';
 
 const router = express.Router();
@@ -13,5 +13,12 @@ router.get('/faq/list', getFaqs);
 router.get('/product', getProducts);
 router.post('/faq/answer/:faqId', postFaqAnswer);
 router.post("/authorize", postAuth);
+
+router.post("/tag-list", uploadTagList);
+router.patch("/tag-list", modifyTagList);
+router.post("/photozone", uploadPhotoZone);
+router.patch("/photozone", modifyTagList);
+router.post("/guideline", uploadGuideline);
+router.patch("/guideline", modifyTagList);
 
 export default router;
