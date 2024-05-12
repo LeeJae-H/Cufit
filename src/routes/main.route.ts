a2b47021c03af36254f1b3a9fd6a661d97ee4795
Wonyoung Jang <nex5turbo@gmail.com>
2024-05-12 17:06:16 +0900

    endpoint slash

--- src/routes/main.route.ts
+++ src/routes/main.route.ts
@@ -8,7 +8,7 @@ router.get("/tag-list", getTagList);
 router.get("/today/guideline", getTodayGuideline);
 router.get("/today/photozone", getTodayPhotozone);
 
-router.get("trending/guidelines");
-router.get("popular/guidelines", getPopularGuidelines);
-router.get("popular/photozones", getPopularPhotozones);
+router.get("/trending/guidelines");
+router.get("/popular/guidelines", getPopularGuidelines);
+router.get("/popular/photozones", getPopularPhotozones);
 export default router;
